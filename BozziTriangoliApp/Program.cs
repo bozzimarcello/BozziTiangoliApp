7e022498fb1a4aa6e81bacedd11fe2114421daf7
Marcello Bozzi <bozzimarcello@gmail.com>
2022-11-08 11:54:46 +0100

    Aggiunti i file di progetto

---
+++ BozziTriangoliApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BozziTriangoliApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Operatori logici
+            // AND &&
+            // OR ||
+            // NOT !
+
+            // Operatori di confronto
+            // UGUALE == (occhio = singolo è assegnazione)
+            // DIVERSO !=
+        }
+    }
+}
